345156923cd0d8b4bd38eae9da6a15966c0cadea
Christopher Guthrie <christopher.guthrie@blackrock.com>
2018-07-31 18:42:54 -0400

    Added TODOs

--- api_consumer.py
+++ api_consumer.py
@@ -2,24 +2,22 @@ import requests
 
 API_BASE_URL = "http://deckofcardsapi.com/api"
 
+# This function should shuffle a deck and return a deck_id from the Deck of Cards API
 def shuffle():
-    response = requests.get("{api}/deck/new/shuffle/?deck_count=1".format(
-        api=API_BASE_URL
-    ))
-    json_parsed_response = response.json()
-    if response.status_code == 200 and json_parsed_response["success"] == True:
-        return json_parsed_response["deck_id"]
-    else:
-        raise Exception("Trouble shuffling the deck!")
     
+    # TODO: Make HTTP request here to shuffle a new deck
+    
+    # TODO: if this was a successful request ...:
+        return json_parsed_data["deck_id"]
+    # else:
+        raise Exception("Trouble shuffling the deck!")
+
+# This function should call the Deck of Cards API
 def draw_cards(deck_id, number_of_cards):
-    response = requests.get("{api}/deck/{deck_id}/draw/?count={number_of_cards}".format(
-        api=API_BASE_URL,
-        deck_id=deck_id,
-        number_of_cards=number_of_cards
-    ))
-    json_parsed_response = response.json()
-    if response.status_code == 200 and json_parsed_response["success"] == True:
-        return json_parsed_response["cards"]
-    else:
+    
+    # TODO: Make HTTP request here to draw {number_of_cards} cards from deck with id {deck_id}
+
+    # TODO: if this was a successful request ...:
+        return json_parsed_data["cards"]
+    # else:
         raise Exception("Error drawing cards (greasy fingers maybe)!")
