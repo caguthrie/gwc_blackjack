dcbacbb783373dc7c4fe2e4f1bd48bbb6ce5a6af
Emily Tran <e99tran@gmail.com>
2018-08-02 14:01:41 -0400

    finished versions with input updated for python 3.6

--- api_consumer.py
+++ api_consumer.py
@@ -4,20 +4,25 @@ API_BASE_URL = "http://deckofcardsapi.com/api"
 
 # This function should shuffle a deck and return a deck_id from the Deck of Cards API
 def shuffle():
+
+    newDeck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     
-    # TODO: Make HTTP request here to shuffle a new deck
-    
-    # TODO: if this was a successful request ...:
-        return json_parsed_data["deck_id"]
-    # else:
+    # if this was a successful request:
+    if newDeck.status_code == 200:
+        return newDeck.json().get("deck_id")
+    else:
         raise Exception("Trouble shuffling the deck!")
 
-# This function should call the Deck of Cards API
-def draw_cards(deck_id, number_of_cards):
-    
-    # TODO: Make HTTP request here to draw {number_of_cards} cards from deck with id {deck_id}
 
-    # TODO: if this was a successful request ...:
-        return json_parsed_data["cards"]
-    # else:
+
+
+# This function should call the Deck of Cards API
+def draw_cards(deck_id, number_of_cards):    
+    url = "https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=" + str(number_of_cards)
+    cards = requests.get(url)
+
+    # if this was a successful request:
+    if cards.status_code == 200:
+        return cards.json().get("cards")
+    else:
         raise Exception("Error drawing cards (greasy fingers maybe)!")
