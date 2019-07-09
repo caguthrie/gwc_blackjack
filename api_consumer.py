b7d7abaead06245dee8b03cacc89483d94f86c29
Christopher Guthrie <christopher.guthrie@blackrock.com>
2019-07-09 13:28:11 -0400

    Minor refactoring

--- api_consumer.py
+++ api_consumer.py
@@ -5,11 +5,11 @@ API_BASE_URL = "http://deckofcardsapi.com/api"
 # This function should shuffle a deck and return a deck_id from the Deck of Cards API
 def shuffle():
 
-    newDeck = requests.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    new_deck = requests.get(API_BASE_URL + "/deck/new/shuffle/?deck_count=1")
 
     # if this was a successful request:
-    if newDeck.status_code == 200:
-        return newDeck.json().get("deck_id")
+    if new_deck.status_code == 200:
+        return new_deck.json().get("deck_id")
     else:
         raise Exception("Trouble shuffling the deck!")
 
@@ -18,7 +18,7 @@ def shuffle():
 
 # This function should call the Deck of Cards API and return the cards drawn
 def draw_cards(deck_id, number_of_cards):
-    url = "http://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=" + str(number_of_cards)
+    url = API_BASE_URL + "/deck/" + deck_id + "/draw/?count=" + str(number_of_cards)
     cards = requests.get(url)
 
     # if this was a successful request:
